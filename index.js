5f45e0d8eb9353d4c077c2be22575aea06676163
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-11-15 06:14:11 +0900

    navbar comleted

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+});
+
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
   burger.addEventListener('click', () => {
+    // toggle nav
     nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.0}s`;
+      }
+    });
+    // burger Animation
+    burger.classList.toggle('toggle');
   });
 }
 
